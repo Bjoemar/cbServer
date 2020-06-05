e0142baee1a5df4ab547916bbeab919e35c11be3
bjoemar <bacleaanjoemar22@gmail.com>
2020-06-05 10:38:37 +0800

    change server port

--- secret.js
+++ secret.js
@@ -1,5 +1,5 @@
 const secret = {
-	url : 'mongodb://127.0.0.1:27017/chatbox',
+	url : 'mongodb+srv://joemar12:joemar12@torrent-oh6ud.mongodb.net/joemar12?retryWrites=true&w=majority',
 	secretKey : 'ChatBoxApp',
 }
 
